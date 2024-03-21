a1e17605ee4248504baa57737571912da881dffa
girlfriend-deficit <girlfrienddeficit@skiff.com>
2024-03-21 01:19:50 -0400

    huge win, totally replicating encryption

--- src/keystore/encryption.rs
+++ src/keystore/encryption.rs
@@ -13,6 +13,7 @@ pub fn encrypt(data: &str, encryption_type: EncryptionType) -> String {
 }
 
 pub fn encrypt_nacl(data: &str, password: &str) -> String {
+    sodiumoxide::init().unwrap();
     let NACL_SALT = [
         0x13, 0x71, 0x83, 0xdf, 0xf1, 0x5a, 0x09, 0xbc, 0x9c, 0x90, 0xb5, 0x51, 0x87, 0x39, 0xe9,
         0xb1,
@@ -36,12 +37,19 @@ pub fn encrypt_nacl(data: &str, password: &str) -> String {
 
     let key = secretbox::Key::from_slice(&key_bytes).unwrap();
 
-    let nonce = secretbox::gen_nonce();
-    println!("nonce: {:?}", nonce);
-    let encrypted_data = secretbox::seal(data.as_bytes(), &nonce, &key);
+    let nonce = secretbox::Nonce([
+        139, 30, 112, 189, 93, 199, 196, 156, 252, 64, 20, 97, 225, 132, 77, 52, 125, 179, 224, 39,
+        35, 194, 154, 228,
+    ]);
+
+    let encrypted_data = secretbox::seal(&mut data.as_bytes(), &nonce, &key);
+
+    let mut output = Vec::new();
+    output.extend_from_slice(&nonce.0);
+    output.extend_from_slice(&encrypted_data);
 
     // Convert the encrypted data to a hexadecimal string
-    let formatted_output = encrypted_data
+    let formatted_output = output
         .iter()
         .map(|&b| format!("{:02x}", b))
         .collect::<String>();
